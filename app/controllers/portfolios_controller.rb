8e92d6fcf1ee91bbb552d1b9e6d35349cd9fef65
Julius Simtim <juliussimtim@gmail.com>
2018-10-24 15:39:52 -0700

    Integrated new and create actions for portfolios

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -2,4 +2,23 @@ class PortfoliosController < ApplicationController
 	def index
 		@portfolio_items = Portfolio.all
 	end
+
+	def new
+		@portfolio_item = Portfolio.new
+	end
+
+ 	def create
+	    @portfolio_item = Portfolio.new(params.require(:porfolio).permit(:title, :subtitle, :body))
+
+	    respond_to do |format|
+	      if @blog.save
+	        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
+	        format.json { render :show, status: :created, location: @blog }
+	      else
+	        format.html { render :new }
+	        format.json { render json: @blog.errors, status: :unprocessable_entity }
+	      end
+	    end
+	end
+
 end
