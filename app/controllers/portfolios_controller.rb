32735c5ba6b2550fff8749dde77146bb74befec0
Julius Simtim <juliussimtim@gmail.com>
2018-10-24 17:50:05 -0700

    Implemented edit and update functionality

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -11,12 +11,26 @@ class PortfoliosController < ApplicationController
 	    @portfolio_item = Portfolio.new(params.require(:porfolio).permit(:title, :subtitle, :body))
 
 	    respond_to do |format|
-	      if @blog.save
-	        format.html { redirect_to @blog, notice: 'Blog was successfully created.' }
-	        format.json { render :show, status: :created, location: @blog }
+	      if @@portfolio_item.save
+	        format.html { redirect_to portfolios_path, notice: 'Your porfolio item was successfully created.' }
 	      else
 	        format.html { render :new }
-	        format.json { render json: @blog.errors, status: :unprocessable_entity }
+	      end
+	    end
+	end
+
+	def edit
+		@portfolio_item = Portfolio.find(params[:id])
+	end
+
+	def update
+		@portfolio_item = Portfolio.find(params[:id])
+
+	    respond_to do |format|
+	      if @portfolio_item.update(params.require(:portfolio).permit(:title, :subtitle, :body))
+	        format.html { redirect_to portfolios_path, notice: 'The record was successfully updated.' }
+	      else
+	        format.html { render :edit }
 	      end
 	    end
 	end
